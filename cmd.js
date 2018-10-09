f709f853175570c0f254e24002b3cfdd19a041b2
Mike Thomas <mthomas@tokbox.com>
2018-10-09 15:30:28 +0200

    Refactor with eslint and remove opentok dependency

--- cmd.js
+++ cmd.js
@@ -1,67 +1,34 @@
 #!/usr/bin/env node
 
-const args = require('args'),
-  { generateToken } = require('opentok-jwt'),
-  options = args.Options.parse([{
-    name: 'apikey',
-    shortName: 'a',
-    help: 'An OpenTok API Key'
-  }, {
-    name: 'secret',
-    shortName: 's',
-    help: 'An OpenTok API Secret'
-  }, {
-    name: 'issuerType',
-    shortName: 'i',
-    help: 'Optional issuer type, one of project or account',
-    defaultValue: 'project'
-  }, {
-    name: 'expires',
-    shortName: 'e',
-    type: 'int',
-    help: 'Optional expire time for the token in milliseconds (30 days from now by default)'
-  }, {
-    name: 'env',
-    shortName: 'v',
-    defaultValue: 'prod',
-    help: 'Optional environment parameter "prod", "dev", "rel" or API URL'
-  }]),
-  opts = args.parser(process.argv).parse(options),
-  OpenTok = require('opentok'),
-  apiKey = opts.apikey,
-  secret = opts.secret,
-  issuerType = opts.issuerType;
+const args = require('args');
+const { generateToken } = require('opentok-jwt');
 
-if (!apiKey || !secret) {
-  errorMessage();
-  return;
-}
-
-let opentok;
-if (opts.env !== 'prod') {
-  let apiUrl;
-  switch (opts.env) {
-    case 'dev':
-      apiUrl = 'https://anvil-tbdev.opentok.com';
-    break;
-    case 'rel':
-      apiUrl = 'https://anvil-tbrel.opentok.com';
-    break;
-    default:
-      apiUrl = opts.env;
-    break;
-  }
-  opentok = new OpenTok(apiKey, secret, apiUrl);
-} else {
-  opentok = new OpenTok(apiKey, secret);
-}
-
-const token = generateToken(apiKey, secret, issuerType, opt.expires);
-outputResults(token);
-
-function outputResults(token) {
-  console.info('token: ', token);
-}
+const options = args.Options.parse([{
+  name: 'apikey',
+  shortName: 'a',
+  help: 'An OpenTok API Key',
+}, {
+  name: 'secret',
+  shortName: 's',
+  help: 'An OpenTok API Secret',
+}, {
+  name: 'issuerType',
+  shortName: 'i',
+  help: 'Optional issuer type, one of project or account',
+  defaultValue: 'project',
+}, {
+  name: 'expires',
+  shortName: 'e',
+  type: 'int',
+  help: 'Optional expire time for the token in milliseconds (30 days from now by default)',
+}, {
+  name: 'env',
+  shortName: 'v',
+  defaultValue: 'prod',
+  help: 'Optional environment parameter "prod", "dev", "rel" or API URL',
+}]);
+const opts = args.parser(process.argv).parse(options);
+const { apiKey, secret, issuerType } = opts;
 
 function errorMessage(err) {
   if (err) {
@@ -73,4 +40,15 @@ function errorMessage(err) {
   }
   console.error(options.getHelp());
   process.exit(1);
+}
+
+function outputResults(token) {
+  console.info('token: ', token);
+}
+
+if (!apiKey || !secret) {
+  errorMessage();
+} else {
+  const token = generateToken(apiKey, secret, issuerType, opts.expires);
+  outputResults(token);
 }
